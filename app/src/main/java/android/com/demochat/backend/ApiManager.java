8157014ff9cf8c828161d83b3e8cf3e8abe807d4
amit rai <bipulamit@gmail.com>
2016-05-25 12:14:32 +0530

    Added new packages

---
+++ app/src/main/java/android/com/demochat/backend/ApiManager.java
@@ -0,0 +1,8 @@
+package android.com.demochat.backend;
+
+/**
+ * Created by amitrai on 25/5/16.
+ */
+
+public class ApiManager {
+}
---
+++ app/src/main/java/android/com/demochat/utility/AppConstants.java
@@ -0,0 +1,9 @@
+package android.com.demochat.utility;
+
+/**
+ * Created by amitrai on 25/5/16.
+ */
+
+public class AppConstants {
+    public static final String BASE_URL ="192.168.1.91";
+}
